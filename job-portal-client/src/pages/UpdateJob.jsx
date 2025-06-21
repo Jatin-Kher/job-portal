a48a22c4031df1413a852a1658a7aa6afdf45668
Jatin-Kher <144918681+Jatin-Kher@users.noreply.github.com>
2025-06-21 17:42:19 +0530

    Update UpdateJob.jsx

--- job-portal-client/src/pages/UpdateJob.jsx
+++ job-portal-client/src/pages/UpdateJob.jsx
@@ -37,7 +37,7 @@ const UpdateJob = () => {
     data.skills = selectedOption;
     // console.log(data)
 
-    fetch(`https://mern-jobportal-ckfs.onrender.com/update-job/${id}`, {
+    fetch(`https://job-portal-backend-nhiy.onrender.com/update-job/${id}`, {
       method: "PATCH",
       headers: {
         "Content-type": "application/json",
